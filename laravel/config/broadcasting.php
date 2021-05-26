b84d52900e31b97d0827bf22300f26a3e5180cb2
Ryan Nugraha <pnugraha89@gmail.com>
2021-05-26 21:58:40 +1000

    test

--- laravel/config/broadcasting.php
+++ laravel/config/broadcasting.php
@@ -11,11 +11,11 @@ return [
     | framework when an event needs to be broadcast. You may set this to
     | any of the connections defined in the "connections" array below.
     |
-    | Supported: "pusher", "redis", "log", "null"
+    | Supported: "pusher", "ably", "redis", "log", "null"
     |
-    */
+     */
 
-    'default' => env('BROADCAST_DRIVER', 'null'),
+    'default'     => env('BROADCAST_DRIVER', 'null'),
 
     /*
     |--------------------------------------------------------------------------
@@ -26,34 +26,40 @@ return [
     | to broadcast events to other systems or over websockets. Samples of
     | each available type of connection are provided inside this array.
     |
-    */
+     */
 
     'connections' => [
 
         'pusher' => [
-            'driver' => 'pusher',
-            'key' => env('PUSHER_APP_KEY'),
-            'secret' => env('PUSHER_APP_SECRET'),
-            'app_id' => env('PUSHER_APP_ID'),
+            'driver'  => 'pusher',
+            'key'     => env('PUSHER_APP_KEY'),
+            'secret'  => env('PUSHER_APP_SECRET'),
+            'app_id'  => env('PUSHER_APP_ID'),
+            'host'    => 'php-laravel-websocket',
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'useTLS' => true,
-            ],
+                'useTLS'  => true
+            ]
         ],
 
-        'redis' => [
-            'driver' => 'redis',
-            'connection' => 'default',
+        'ably'   => [
+            'driver' => 'ably',
+            'key'    => env('ABLY_KEY')
         ],
 
-        'log' => [
-            'driver' => 'log',
+        'redis'  => [
+            'driver'     => 'redis',
+            'connection' => 'default'
         ],
 
-        'null' => [
-            'driver' => 'null',
+        'log'    => [
+            'driver' => 'log'
         ],
 
-    ],
+        'null'   => [
+            'driver' => 'null'
+        ]
+
+    ]
 
 ];
